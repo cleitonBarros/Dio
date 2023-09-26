c8a5158166b5ddd74d3bf65e3c45ca4024a8ea96
Cleiton barros <ton.barros123@hotmail.com>
2023-09-26 00:08:31 -0300

    :white_check_mark: feat: update header e footer

---
+++ profile/src/components/Footer.tsx
@@ -0,0 +1,20 @@
+import { ComponentProps } from "react";
+
+import { tv, VariantProps } from "tailwind-variants";
+
+export type FooterProps = ComponentProps<"footer"> &
+  VariantProps<typeof footer>;
+
+const footer = tv({
+  base: "flex justify-center items-center p-6 "
+});
+export function Footer({ ...props }: FooterProps) {
+  return (
+    <footer className={footer()} {...props}>
+      <article className="flex justify-center items-center  gap-4 w-full max-w-7xl ">
+        <p className=" text-black text-base">@2023</p>
+        <div className="h-px w-[60rem] bg-slate-500"></div>
+      </article>
+    </footer>
+  );
+}
--- profile/src/components/Header.tsx
+++ profile/src/components/Header.tsx
@@ -7,16 +7,18 @@ export type HeaderProps = ComponentProps<"header"> &
   VariantProps<typeof header>;
 
 const header = tv({
-  base: "flex justify-center items-center gap-4 p-6"
+  base: "flex justify-center items-center p-6"
 });
 
 export function Header({ ...props }: HeaderProps) {
   return (
     <header className={header()} {...props}>
-      <div className="h-px w-9/12 bg-slate-500"></div>
-      <a href="#">
-        <Instagram className="w-6" />
-      </a>
+      <div className="flex justify-center items-center gap-4 w-full max-w-7xl ">
+        <div className="h-px w-[60rem] bg-slate-500"></div>
+        <a href="#">
+          <Instagram className="w-6" />
+        </a>
+      </div>
     </header>
   );
 }
