ab0fc73c903b4793b5435363fbd0d2d5ad3f422f
Cleiton barros <ton.barros123@hotmail.com>
2023-09-26 00:09:28 -0300

    :lipstick: feat: add font-family fredoka and poppins

--- profile/tailwind.config.js
+++ profile/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {}
+    extend: {},
+    fontFamily: {
+      Fredoka: "'Fredoka', sans-serif",
+      Poppins: "'Poppins', sans-serif"
+    }
   },
   plugins: []
 };
