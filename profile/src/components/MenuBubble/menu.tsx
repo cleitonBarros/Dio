6aa253ed79eae3d42a44781cef16cf4489c31584
Cleiton barros <ton.barros123@hotmail.com>
2023-09-30 21:57:28 -0300

    :Sparkles: feat: add ternario

--- profile/src/components/MenuBubble/menu.tsx
+++ profile/src/components/MenuBubble/menu.tsx
@@ -9,58 +9,64 @@ export function Menu() {
   const { pathname } = location;
 
   return (
-    <div className="about ">
-      {path(pathname) === "/" ? (
-        <a
-          title="Trabalhos"
-          className="bg_links page position-1 Work   "
-          href="/works"
-        >
-          <span className="icon"></span>
-        </a>
+    <>
+      {pathname === "/" ? (
+        ""
       ) : (
-        <a title="Home" className="bg_links page position-1 Home " href="/">
-          <span className="icon"></span>
-        </a>
-      )}
+        <div className="about ">
+          {path(pathname) === "/" ? (
+            <a
+              title="Trabalhos"
+              className="bg_links page position-1 Work   "
+              href="/works"
+            >
+              <span className="icon"></span>
+            </a>
+          ) : (
+            <a title="Home" className="bg_links page position-1 Home " href="/">
+              <span className="icon"></span>
+            </a>
+          )}
 
-      {path(pathname) === "about" ? (
-        <a
-          title="Trabalhos"
-          className="bg_links page position-2 Work  "
-          href="/works"
-        >
-          <span className="icon"></span>
-        </a>
-      ) : (
-        <a
-          title="Sobre"
-          className="bg_links page position-2 About  "
-          href="/about"
-        >
-          <span className="icon"></span>
-        </a>
-      )}
+          {path(pathname) === "about" ? (
+            <a
+              title="Trabalhos"
+              className="bg_links page position-2 Work  "
+              href="/works"
+            >
+              <span className="icon"></span>
+            </a>
+          ) : (
+            <a
+              title="Sobre"
+              className="bg_links page position-2 About  "
+              href="/about"
+            >
+              <span className="icon"></span>
+            </a>
+          )}
 
-      {path(pathname) === "contact" ? (
-        <a
-          title="Trabalhos"
-          className="bg_links page position-3  Work"
-          href="/works"
-        >
-          <span className="icon"></span>
-        </a>
-      ) : (
-        <a
-          title="Contato"
-          className="bg_links page position-3  Contact"
-          href="/contact"
-        >
-          <span className="icon"></span>
-        </a>
-      )}
+          {path(pathname) === "contact" ? (
+            <a
+              title="Trabalhos"
+              className="bg_links page position-3  Work"
+              href="/works"
+            >
+              <span className="icon"></span>
+            </a>
+          ) : (
+            <a
+              title="Contato"
+              className="bg_links page position-3  Contact"
+              href="/contact"
+            >
+              <span className="icon"></span>
+            </a>
+          )}
 
-      <a className="bg_links logo" title="menu"></a>
-    </div>
+          <a className="bg_links logo" title="menu"></a>
+        </div>
+      )}
+    </>
   );
 }
